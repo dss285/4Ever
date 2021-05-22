1f4c2a1a98c0aa94b21b0004bda81fc20257e746
Dss285 <aeonmoans@gmail.com>
2021-05-22 15:37:30 +0300

    config file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+#4ever
+token = os.getenv('BOT_TOKEN')
+host = os.getenv('DATABASE_HOST')
+user = os.getenv('DATABASE_USER')
+password = os.getenv('DATABASE_PASSWORD')
+database = os.getenv('DATABASE_NAME')
+steam_api_key = os.getenv('STEAM_API_KEY')
+
+asset_path = '/assets'
